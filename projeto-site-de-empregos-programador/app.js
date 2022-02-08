c7ede4b0cd58dd6906dc2e7c87b8000dce1f1582
Lucas Nunes <lucasnunespinto48@gmail.com>
2022-02-08 17:30:11 -0300

    Routes, Models and DB

--- projeto-site-de-empregos-programador/app.js
+++ projeto-site-de-empregos-programador/app.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+const express    = require('express');
+const app        = express();
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
@@ -7,6 +9,28 @@ app.listen(PORT, function() {
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+//body parser
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//db Connection
+
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar", err);
+    });
+
+//Routes
+
+
 app.get('/',(req, res) => {
     res.send("Está funcionando teste");
 });
+
+// jobs routes
+
+app.use('/jobs', require('./routes/jobs'));
